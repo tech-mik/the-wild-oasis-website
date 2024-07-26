efae925f3c89600ebac2bf39603104aaf50ecc99
Mik ten Holt <miktenholt@gmail.com>
2024-07-26 12:25:15 +0200

    initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,32 @@
+import Header from '@/components/Header'
+
+import '@/styles/globals.css'
+
+import { Josefin_Sans } from 'next/font/google'
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const metadata = {
+  title: {
+    template: '%s / The Wild Oasis',
+    default: 'Welcome / The Wild Oasis',
+  },
+  description:
+    'Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased `}>
+        <Header />
+        <div className='flex-1 px-8 py-12 grid '>
+          <main className='max-w-7xl mx-auto w-full'>{children}</main>
+        </div>
+      </body>
+    </html>
+  )
+}
